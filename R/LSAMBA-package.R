e1a224fdbc3a98814086b19ee6137750d115c3f3
Auriane Gabaut <122619227+aurianegbt@users.noreply.github.com>
2025-03-21 14:34:07 +0100

    update import for futur update

--- R/LSAMBA-package.R
+++ R/LSAMBA-package.R
@@ -15,5 +15,12 @@
 #' @importFrom stats anova
 #' @importFrom stats as.formula
 #' @importFrom stats logLik
+#' @importFrom dplyr %>%
+#' @importFrom dplyr arrange
+#' @importFrom dplyr bind_rows
+#' @importFrom dplyr filter
+#' @importFrom dplyr mutate
+#' @importFrom dplyr rename
+#' @importFrom dplyr select
 ## usethis namespace: end
 NULL
